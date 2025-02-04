2242c04b3311e9ab782f9f402a8343a04c1e2b2d
ChrysArex <chrysazokly@gmail.com>
2025-02-04 23:58:39 +0100

    creation of the game server with connection, session and communication enable

--- server.js
+++ server.js
@@ -12,19 +12,41 @@ class GameServer {
   }
 
   newGame(ws, req) {
+    //console.log(req.headers["playerid"]);
+    this.initSession(ws, req.headers["playerid"]); //manage session for the new connection
+    this.setUpListeners(ws, req); //set liteners for the new connection
+  }
+
+  initSession(ws, playerId) {
     if (
       Object.keys(this.sessions).length !== 0 &&
       this.sessions[this.ssId].length < 6
     ) {
-      this.sessions[this.ssId].push(ws);
+      this.sessions[this.ssId].push({ ws: ws, playerId: playerId });
+      ws.send(JSON.stringify({ type: "ssId", content: this.ssId }));
       console.log(
         `new player added, the sessions so far: ${this.sessions[this.ssId]}`,
       );
     } else {
       this.ssId = crypto.randomUUID();
-      this.sessions[this.ssId] = [ws];
+      this.sessions[this.ssId] = [{ ws: ws, playerId: playerId }];
+      ws.send(JSON.stringify({ type: "ssId", content: this.ssId }));
       console.log(`new sessions created, sessions id: ${this.ssId}`);
     }
+  }
+
+  setUpListeners(ws, req) {
+    ws.on("message", (msg) => {
+      console.log("Message broadcasted");
+      this.broadcast(msg, this.sessions[JSON.parse(msg).sessionId]);
+    });
+  }
+
+  broadcast(message, players) {
+    players.forEach((player) => {
+      if (player["playerId"] !== JSON.parse(message).senderId)
+        player.ws.send(JSON.stringify(JSON.parse(message).content));
+    });
   }
 
   listen(port, callback) {
--- ws_quick_client.js
+++ ws_quick_client.js
@@ -1,3 +1,25 @@
-import WebSocket from 'ws';
+import WebSocket from "ws";
 
-const player = new WebSocket('ws://127.0.0.1:3000/newGame');
+const id = crypto.randomUUID();
+console.log(`my ID: ${id}`);
+const player = new WebSocket("ws://127.0.0.1:3000/newGame", {
+  headers: { playerid: id },
+});
+let sessionId = null;
+process.stdin.on("data", (data) => {
+  player.send(
+    JSON.stringify({
+      type: "action",
+      senderId: id,
+      sessionId: sessionId,
+      content: data.toString(),
+    }),
+  );
+});
+
+player.on("open", () => console.log("I am connected"));
+player.on("message", (message) => {
+  let msg = JSON.parse(message);
+  if (msg.type === "ssId") sessionId = msg.content;
+  console.log(`Message: ${msg}`);
+});
