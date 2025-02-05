3a38c0941f3c089ab2bacefd3d82b7440e5185d0
ChrysArex <chrysazokly@gmail.com>
2025-02-05 23:51:34 +0100

    enable separated message broadcasting for each team

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ class GameServer {
     this.sessions = { init: 0 };
     this.ssId = "init";
     this.server = app ? app : express();
+    this.typeIdx = -1;
     expressWs(this.server);
     this.server.get("/hello", (req, res) => res.send("hello my gee\n"));
     this.server.ws("/newGame", this.newGame.bind(this));
@@ -14,38 +15,74 @@ class GameServer {
   newGame(ws, req) {
     //console.log(req.headers["playerid"]);
     this.initSession(ws, req.headers["playerid"]); //manage session for the new connection
-    this.setUpListeners(ws, req); //set liteners for the new connection
+    this.setUpListeners(ws, req.headers["playerid"]); //set liteners for the new connection
   }
 
   initSession(ws, playerId) {
+    this.typeIdx += 1;
+    const playerType = this.typeIdx % 2 === 0 ? "vessel" : "opponent";
     if (
       Object.keys(this.sessions).length !== 0 &&
       this.sessions[this.ssId].length < 6
     ) {
-      this.sessions[this.ssId].push({ ws: ws, playerId: playerId });
-      ws.send(JSON.stringify({ type: "ssId", content: this.ssId }));
+      this.sessions[this.ssId].push({
+        ws: ws,
+        playerId: playerId,
+        playerType: playerType,
+      });
+
+      ws.send(
+        JSON.stringify({
+          type: "init",
+          ssId: this.ssId,
+          playerType: playerType,
+        }),
+      );
+
       console.log(
         `new player added, the sessions so far: ${this.sessions[this.ssId]}`,
       );
     } else {
       this.ssId = crypto.randomUUID();
-      this.sessions[this.ssId] = [{ ws: ws, playerId: playerId }];
-      ws.send(JSON.stringify({ type: "ssId", content: this.ssId }));
+      this.sessions[this.ssId] = [
+        { ws: ws, playerId: playerId, playerType: playerType },
+      ];
+
+      ws.send(
+        JSON.stringify({
+          type: "init",
+          ssId: this.ssId,
+          playerType: playerType,
+        }),
+      );
+
       console.log(`new sessions created, sessions id: ${this.ssId}`);
     }
   }
 
-  setUpListeners(ws, req) {
+  setUpListeners(ws, playerId) {
     ws.on("message", (msg) => {
       console.log("Message broadcasted");
       this.broadcast(msg, this.sessions[JSON.parse(msg).sessionId]);
+    });
+
+    ws.on("close", () => {
+      console.log(`Player ${playerId} disconnected`);
     });
   }
 
   broadcast(message, players) {
     players.forEach((player) => {
-      if (player["playerId"] !== JSON.parse(message).senderId)
-        player.ws.send(JSON.stringify(JSON.parse(message).content));
+      if (player["playerId"] !== JSON.parse(message).senderId) {
+        if (player.playerType === JSON.parse(message).playerType)
+          player.ws.send(JSON.stringify(JSON.parse(message).content));
+        else
+          player.ws.send(
+            JSON.stringify(
+              "A message has been sent by a guy of the other team, here is you version of it!",
+            ),
+          );
+      }
     });
   }
 
--- ws_quick_client.js
+++ ws_quick_client.js
@@ -1,15 +1,18 @@
 import WebSocket from "ws";
 
 const id = crypto.randomUUID();
+let sessionId = null;
+let playerType = null;
+
 console.log(`my ID: ${id}`);
 const player = new WebSocket("ws://127.0.0.1:3000/newGame", {
   headers: { playerid: id },
 });
-let sessionId = null;
 process.stdin.on("data", (data) => {
   player.send(
     JSON.stringify({
       type: "action",
+      playerType: playerType,
       senderId: id,
       sessionId: sessionId,
       content: data.toString(),
@@ -20,6 +23,9 @@ process.stdin.on("data", (data) => {
 player.on("open", () => console.log("I am connected"));
 player.on("message", (message) => {
   let msg = JSON.parse(message);
-  if (msg.type === "ssId") sessionId = msg.content;
+  if (msg.type === "init") {
+    sessionId = msg.ssId;
+    playerType = msg.playerType;
+  }
   console.log(`Message: ${msg}`);
 });
